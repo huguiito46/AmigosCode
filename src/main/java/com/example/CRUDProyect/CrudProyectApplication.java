a5ba818b10a27721a1020388b5f73b1ebef46c5e
Hugo <hugogonzalezc.cmi@gmail.com>
2023-02-06 13:37:23 +0100

    Starting proyect CRUD

---
+++ src/main/java/com/example/CRUDProyect/CrudProyectApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRUDProyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudProyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CRUDProyect/CrudProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUDProyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
